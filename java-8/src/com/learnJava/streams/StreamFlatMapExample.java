01250a28b04052f82d04006edfd977798fec6853
Luis Nakano <lgnakano@gmail.com>
2022-02-26 22:32:28 -0500

    Return list of activities by decreasing frequency. In case of same frequency, alphabetically.

--- java-8/src/com/learnJava/streams/StreamFlatMapExample.java
+++ java-8/src/com/learnJava/streams/StreamFlatMapExample.java
@@ -3,27 +3,57 @@ package com.learnJava.streams;
 import com.learnJava.data.Student;
 import com.learnJava.data.StudentDataBase;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.toList;
 
 public class StreamFlatMapExample {
 
+    public static Map<String, Long> studentActivitiesWithCount() {
+
+        return StudentDataBase.getAllStudents()
+                .stream()
+                .map(Student::getActivities) //Stream<List<String>>
+                .flatMap(List::stream) //<Stream<String>
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting())) ;
+//        return studentActivities;
+
+    }
+
+    public static List<Map.Entry<String, Long>> studentActivitiesWithCountSorted() {
+        return studentActivitiesWithCount()
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByKey())
+                .collect(Collectors.toList());
+    }
+
+    public static List<Map.Entry<String, Long>> studentActivitiesWithCountSortedByFreqDec() {
+        return studentActivitiesWithCount()
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByKey())
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .collect(Collectors.toList());
+    }
+
     public static List<String> printStudentActivities() {
 
-        List<String> studentActivities = StudentDataBase.getAllStudents()
+        return StudentDataBase.getAllStudents()
                 .stream()
                 .map(Student::getActivities) //Stream<List<String>>
                 .flatMap(List::stream) //<Stream<String>
                 .collect(toList());
 
-        return studentActivities;
-
     }
 
     public static List<String> printUniqueStudentActivities() {
 
-        List<String> studentActivities = StudentDataBase.getAllStudents()
+        return StudentDataBase.getAllStudents()
                 .stream()
                 .map(Student::getActivities)
                 .flatMap(List::stream)
@@ -31,20 +61,16 @@ public class StreamFlatMapExample {
                 .sorted()
                 .collect(toList());
 
-        return studentActivities;
-
     }
 
     public static long getStudentActivitiesCount() {
 
-        long totalActivities = StudentDataBase.getAllStudents()
+        return StudentDataBase.getAllStudents()
                 .stream()
                 .map(Student::getActivities)
                 .flatMap(List::stream)
                 .distinct()
                 .count();
-
-        return totalActivities;
 
     }
 
@@ -54,7 +80,9 @@ public class StreamFlatMapExample {
         System.out.println("Student Activities : " + printStudentActivities());
         System.out.println("Unique Student Activities : " + printUniqueStudentActivities());
         System.out.println("Unique Student Activities Count: " + getStudentActivitiesCount());
-
+        System.out.println("Student Activities with count: " + studentActivitiesWithCount());
+        System.out.println("Student Activities with count, sorted: " + studentActivitiesWithCountSorted());
+        System.out.println("Student Activities with count, sorted by decreasing frequency: " + studentActivitiesWithCountSortedByFreqDec());
     }
 
 }
