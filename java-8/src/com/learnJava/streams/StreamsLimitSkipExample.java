011c4516b3329b42208524f6a6648b9594d05862
Luis Nakano <lgnakano@gmail.com>
2022-03-01 16:21:03 -0500

    Remove warnings.

--- java-8/src/com/learnJava/streams/StreamsLimitSkipExample.java
+++ java-8/src/com/learnJava/streams/StreamsLimitSkipExample.java
@@ -9,14 +9,14 @@ public class StreamsLimitSkipExample {
     public static Optional<Integer> limit(List<Integer> integers){
         return  integers.stream()
                 .limit(2)
-                .reduce((a,b)-> a+b);
+                .reduce(Integer::sum);
 
     }
 
     public static Optional<Integer> skip(List<Integer> integers){
         return  integers.stream()
                 .skip(3)
-                .reduce((a,b)-> a+b);
+                .reduce(Integer::sum);
     }
     
 
@@ -25,11 +25,11 @@ public class StreamsLimitSkipExample {
         List<Integer> integers = Arrays.asList(6,7,8,9,10);
 
         Optional<Integer> limitResult = limit(integers);
-        int result = limitResult.isPresent() ? limitResult.get() : 0;
+        int result = limitResult.orElse(0);
         System.out.println("The limit result is : " + result);
 
         Optional<Integer> skipResult = skip(integers);
-        int result1 = skipResult.isPresent() ? skipResult.get() : 0;
+        int result1 = skipResult.orElse(0);
         System.out.println("The skip result is : " + result1);
 
     }
