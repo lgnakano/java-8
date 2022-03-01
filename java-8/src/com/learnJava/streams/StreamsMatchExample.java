f2b108cdca33082b51836444191868d9ceae88e8
Luis Nakano <lgnakano@gmail.com>
2022-03-01 16:58:55 -0500

    Remove warnings.

--- java-8/src/com/learnJava/streams/StreamsMatchExample.java
+++ java-8/src/com/learnJava/streams/StreamsMatchExample.java
@@ -7,26 +7,20 @@ public class StreamsMatchExample {
 
     public static boolean allMatch(){
 
-        boolean result = StudentDataBase.getAllStudents().stream()
+        return StudentDataBase.getAllStudents().stream()
                 .allMatch(student -> student.getGpa()>=3.9);
-
-        return result;
     }
 
     public static boolean anyMatch(){
 
-        boolean result = StudentDataBase.getAllStudents().stream()
+        return StudentDataBase.getAllStudents().stream()
                 .anyMatch(student -> student.getGpa()>=3.9);
-
-        return result;
     }
 
     public static boolean noneMatch(){
 
-        boolean result = StudentDataBase.getAllStudents().stream()
+        return StudentDataBase.getAllStudents().stream()
                 .noneMatch(student -> student.getGpa()>=3.9);
-
-        return result;
     }
 
     public static void main(String[] args) {
