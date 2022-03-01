c39930d57cccd47ba4e79ca13f191e83fc7368c9
Luis Nakano <lgnakano@gmail.com>
2022-03-01 17:12:53 -0500

    Peek inside stream and analyze the results if using in parallel.

--- java-8/src/com/learnJava/streams/StreamsFindAnyFirstExample.java
+++ java-8/src/com/learnJava/streams/StreamsFindAnyFirstExample.java
@@ -7,30 +7,34 @@ import java.util.Optional;
 
 public class StreamsFindAnyFirstExample {
 
-    public static Optional<Student> findAny(){
+    public static Optional<Student> findAnyStudent(){
 
         return StudentDataBase.getAllStudents().stream()
-                .filter(student -> student.getGpa()>=3.8)
+                .parallel()
+                .peek(System.out::println)
+                .filter(student -> student.getGpa()>=3.9)
                 .findAny();
     }
 
-    public static Optional<Student> findFirst(){
+    public static Optional<Student> findFirstStudent(){
 
         return StudentDataBase.getAllStudents().stream()
-                .filter(student -> student.getGpa()>=3.8)
+                .parallel()
+                .peek(System.out::println)
+                .filter(student -> student.getGpa()>=3.9)
                 .findFirst();
     }
 
     public static void main(String[] args) {
 
-        Optional<Student> findAnyStudent = findAny();
+        Optional<Student> findAnyStudent = findAnyStudent();
         if(findAnyStudent.isPresent()){
             System.out.println("Student is :" + findAnyStudent.get());
         }else{
             System.out.println("No Student Found");
         }
 
-        Optional<Student> findFirst = findFirst();
+        Optional<Student> findFirst = findFirstStudent();
         if(findFirst.isPresent()){
             System.out.println("Student is :" + findFirst.get());
         }else{
