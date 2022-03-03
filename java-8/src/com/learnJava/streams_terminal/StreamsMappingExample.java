69f7c5f8a409120c57d0783d75f47da999a15d4f
Luis Nakano <lgnakano@gmail.com>
2022-03-03 14:34:53 -0500

    Alternative way of using collect (using map().collect() instead of .collect(mapping.

--- java-8/src/com/learnJava/streams_terminal/StreamsMappingExample.java
+++ java-8/src/com/learnJava/streams_terminal/StreamsMappingExample.java
@@ -12,17 +12,31 @@ public class StreamsMappingExample {
 
     public static void main(String[] args) {
 
-       Set<String> namesSet = StudentDataBase.getAllStudents()
+        Set<String> namesSet = StudentDataBase.getAllStudents()
                 .stream()
                 .collect(mapping(Student::getName,toSet())); // this avoids the additional map intermediate operation.
 
         System.out.println("namesSet : " + namesSet);
+
+        Set<String> namesSet2 = StudentDataBase.getAllStudents()
+                .stream()
+                .map(Student::getName)
+                .collect(toSet());
+
+        System.out.println("namesSet : " + namesSet2);
 
         List<String> namesList = StudentDataBase.getAllStudents()
                 .stream()
                 .collect(mapping(Student::getName,toList())); // this avoids the additional map intermediate operation.
 
         System.out.println("namesList : " + namesList);
+
+        List<String> namesList2 = StudentDataBase.getAllStudents()
+                .stream()
+                .map(Student::getName)
+                .collect(toList()); // this avoids the additional map intermediate operation.
+
+        System.out.println("namesList : " + namesList2);
 
     }
 
