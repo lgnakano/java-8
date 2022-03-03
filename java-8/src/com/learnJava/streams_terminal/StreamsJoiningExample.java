67c3dac61e2e95d8b4ba70bc10a0e8555b1ce5dd
Luis Nakano <lgnakano@gmail.com>
2022-03-03 14:21:01 -0500

    Convert to List and print.

--- java-8/src/com/learnJava/streams_terminal/StreamsJoiningExample.java
+++ java-8/src/com/learnJava/streams_terminal/StreamsJoiningExample.java
@@ -3,7 +3,6 @@ package com.learnJava.streams_terminal;
 import com.learnJava.data.Student;
 import com.learnJava.data.StudentDataBase;
 
-import java.util.Collection;
 import java.util.stream.Collectors;
 
 public class StreamsJoiningExample {
@@ -26,11 +25,16 @@ public class StreamsJoiningExample {
 
         return StudentDataBase.getAllStudents().stream()
                 .map(Student::getName)
-                .collect(Collectors.joining("-","[","]"));
+                .collect(Collectors.joining(", " ,"[","]"));
     }
 
     public static void main(String[] args) {
 
+        System.out.println(StudentDataBase.getAllStudents()
+                .stream()
+                .map(Student::getName)
+                .collect(Collectors.toList())
+        );
         System.out.println("Names : "+ joining());
         System.out.println("Names With Delimiter : "+ joiningWithDelimiter());
         System.out.println("Names With Delimiter Prefix and Suffix : "+ joiningWithDelimiterWithPrefix());
