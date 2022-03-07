6f1c3273ecf9720949200a43594809c62de8f69c
Luis Nakano <lgnakano@gmail.com>
2022-03-07 05:10:36 -0500

    Resolving multiple inheritance conflicts.

--- java-8/src/com/learnJava/defaults/Client14.java
+++ java-8/src/com/learnJava/defaults/Client14.java
@@ -2,6 +2,10 @@ package com.learnJava.defaults;
 
 public class Client14 implements Interface1,Interface4{
 
+    // we must implement methodA, because it is defined in both
+    // Interface1 and Interface4. Otherwise, java would not
+    // be able to resolve it.
+
     public void  methodA(){
         System.out.println("Inside method A "+Client14.class);
     }
