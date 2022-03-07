48d4ec22600241242d34e63ec983f8b4e5ca4554
Luis Nakano <lgnakano@gmail.com>
2022-03-07 05:11:28 -0500

    How default interface resolution works.

--- java-8/src/com/learnJava/defaults/Client123.java
+++ java-8/src/com/learnJava/defaults/Client123.java
@@ -6,6 +6,10 @@ public class Client123 implements Interface1,Interface2,Interface3 {
         System.out.println("Inside method A "+ Client123.class);
     }
 
+    // 1 -> class that implements the interface
+    // 2 -> the sub interface that extends the interface
+    // 3 -> the pareent interface that was extended
+
     public static void main(String[] args) {
 
         Client123 client123 = new Client123();
