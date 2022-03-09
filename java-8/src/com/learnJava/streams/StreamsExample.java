b1b46e49c1f7e9e445d795713c2ad0966135cd9a
Luis Nakano <lgnakano@gmail.com>
2022-03-09 06:49:04 -0500

    Fix warnings.

--- java-8/src/com/learnJava/streams/StreamsExample.java
+++ java-8/src/com/learnJava/streams/StreamsExample.java
@@ -3,10 +3,8 @@ package com.learnJava.streams;
 import com.learnJava.data.Student;
 import com.learnJava.data.StudentDataBase;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -28,30 +26,26 @@ public class StreamsExample {
 
         Map<String,List<String>> studentMap = StudentDataBase.getAllStudents().stream() //.parallelStream dont forger.
                 .filter(gradePredicate)
-                .peek((student) -> {
-                    System.out.println("After 1st filter " + student );
-                })
+                .peek((student) -> System.out.println("After 1st filter " + student ))
                 .filter(gpaPredicate) // Stream<Student>
-                .peek((student) -> {
-                    System.out.println("After 2nd filter " + student );
-                })
+                .peek((student) -> System.out.println("After 2nd filter " + student ))
                 .collect(Collectors.toMap(Student::getName ,Student::getActivities ));
 
         System.out.println("studentMap  : " + studentMap);
 
         List<String> studentActivities = StudentDataBase.getAllStudents().
                 stream() // Stream<Student>
-                .map(Student::getActivities) //<Stream<List<Activites>>
+                .map(Student::getActivities) //<Stream<List<Activities>>
                 .flatMap(List::stream) //<Stream<String>
                 .distinct() // removes duplicates
                 .collect(Collectors.toList()); //collects it to a list.
 
+        System.out.println("Student activities: " + studentActivities );
+
         List<String> namesList = StudentDataBase.getAllStudents().
                 stream() // Stream<Student>
-                .peek((student -> {
-                    System.out.println(student);
-                }))
-                .map(Student::getName) //<Stream<List<Activites>>
+                .peek((System.out::println))
+                .map(Student::getName) //<Stream<List<Activities>>
                 .peek(System.out::println)
                 .distinct() // removes duplicates
                 .collect(Collectors.toList()); //collects it to a list.
