51c07c0f2fce04b7987bfbfbac323a465dbcc547
Luis Nakano <lgnakano@gmail.com>
2022-03-11 19:07:29 -0500

    Remove warnings.

--- java-8/src/com/learnJava/dates/InstantExample.java
+++ java-8/src/com/learnJava/dates/InstantExample.java
@@ -1,20 +1,19 @@
 package com.learnJava.dates;
 
-import java.time.*;
-import java.time.temporal.ChronoField;
-import java.time.temporal.ChronoUnit;
+import java.time.Duration;
+import java.time.Instant;
 
 public class InstantExample {
 
     public static void main(String[] args) {
 
-        Instant instant = Instant.now(); // Represents the date and time in unix timestamp in a machine readable format.
+        Instant instant = Instant.now(); // Represents the date and time in unix timestamp in a machine-readable format.
         System.out.println(instant);
 
         // Interface1 day has 86400 seconds.
         // What is EPOCH ? -> equal to Jan. 1st 1970
         System.out.println("getEpochSecond  : " +instant.getEpochSecond()); //Jan. 1st 1970 -> 0 seconds
-        //it does not support any of the methods which represents the time in human readable format.
+        //it does not support any of the methods which represents the time in human-readable format.
 
         Instant instant1  =Instant.ofEpochMilli(0);
         System.out.println("instant1 : " + instant1);
